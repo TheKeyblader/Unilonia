83c9dc2cccfa21171f87936ba039c58c97146736
Jean-François Pustay <jef97310@gmail.com>
2020-10-25 18:24:17 +0100

    Added Control Catalog Sample

---
+++ Packages/Unilonia/VS Samples~/ControlCatalog/App.xaml.cs
@@ -0,0 +1,85 @@
+using System;
+using Avalonia;
+using Avalonia.Controls.ApplicationLifetimes;
+using Avalonia.Markup.Xaml;
+using Avalonia.Markup.Xaml.Styling;
+using Avalonia.Styling;
+
+namespace ControlCatalog
+{
+    public class App : Application
+    {
+        public static Styles FluentDark = new Styles
+        {
+            new StyleInclude(new Uri("avares://ControlCatalog/Styles"))
+            {
+                Source = new Uri("avares://Avalonia.Themes.Fluent/Accents/FluentDark.xaml")
+            }
+        };
+
+        public static Styles FluentLight = new Styles
+        {
+            new StyleInclude(new Uri("avares://ControlCatalog/Styles"))
+            {
+                Source = new Uri("avares://Avalonia.Themes.Fluent/Accents/FluentLight.xaml")
+            }
+        };
+
+        public static Styles DefaultLight = new Styles
+        {
+            new StyleInclude(new Uri("resm:Styles?assembly=ControlCatalog"))
+            {
+                Source = new Uri("avares://Avalonia.Themes.Fluent/Accents/Base.xaml")
+            },
+            new StyleInclude(new Uri("resm:Styles?assembly=ControlCatalog"))
+            {
+                Source = new Uri("avares://Avalonia.Themes.Fluent/Accents/BaseLight.xaml")
+            },
+            new StyleInclude(new Uri("resm:Styles?assembly=ControlCatalog"))
+            {
+                Source = new Uri("avares://Avalonia.Themes.Default/Accents/BaseLight.xaml")
+            },
+            new StyleInclude(new Uri("resm:Styles?assembly=ControlCatalog"))
+            {
+                Source = new Uri("avares://Avalonia.Themes.Default/DefaultTheme.xaml")
+            }
+        };
+
+        public static Styles DefaultDark = new Styles
+        {
+            new StyleInclude(new Uri("resm:Styles?assembly=ControlCatalog"))
+            {
+                Source = new Uri("avares://Avalonia.Themes.Fluent/Accents/Base.xaml")
+            },
+            new StyleInclude(new Uri("resm:Styles?assembly=ControlCatalog"))
+            {
+                Source = new Uri("avares://Avalonia.Themes.Fluent/Accents/BaseDark.xaml")
+            },
+            new StyleInclude(new Uri("resm:Styles?assembly=ControlCatalog"))
+            {
+                Source = new Uri("avares://Avalonia.Themes.Default/Accents/BaseDark.xaml")
+            },
+            new StyleInclude(new Uri("resm:Styles?assembly=ControlCatalog"))
+            {
+                Source = new Uri("avares://Avalonia.Themes.Default/DefaultTheme.xaml")
+            }
+        };
+
+        public override void Initialize()
+        {
+            Styles.Insert(0, FluentLight);
+
+            AvaloniaXamlLoader.Load(this);
+        }
+
+        public override void OnFrameworkInitializationCompleted()
+        {
+            if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktopLifetime)
+                desktopLifetime.MainWindow = new MainWindow();
+            else if (ApplicationLifetime is ISingleViewApplicationLifetime singleViewLifetime)
+                singleViewLifetime.MainView = new MainView();
+
+            base.OnFrameworkInitializationCompleted();
+        }
+    }
+}
---
+++ Packages/Unilonia/VS Samples~/ControlCatalog/Program.cs
@@ -0,0 +1,22 @@
+﻿using Avalonia;
+using Avalonia.ReactiveUI;
+
+namespace ControlCatalog
+{
+#if NETCOREAPP
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            BuildAvaloniaApp().StartWithClassicDesktopLifetime(args);
+        }
+
+        public static AppBuilder BuildAvaloniaApp() =>
+            AppBuilder.Configure<App>()
+                .UsePlatformDetect()
+                .UseReactiveUI()
+                .LogToDebug();
+    }
+#endif
+}
+
